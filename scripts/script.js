1759dc194be75b3df64b43d804eecd7bf28fea88
theperacar <petricpetar22@gmail.com>
2020-07-01 18:25:53 +0200

    Updating UI

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const gameBoardObj = (() => {
+  const div1 = document.getElementById('b1');
+  const div2 = document.getElementById('b2');
+  const div3 = document.getElementById('b3');
+  const div4 = document.getElementById('b4');
+  const div5 = document.getElementById('b5');
+  const div6 = document.getElementById('b6');
+  const div7 = document.getElementById('b7');
+  const div8 = document.getElementById('b8');
+  const div9 = document.getElementById('b9');
+
+  const gameBoardArr = [div1, div2, div3, div4, div5, div6, div7, div8, div9];
+  console.log(gameBoardArr);
+
+})();
+
+//const playerobj 
