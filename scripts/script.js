0c0e114363787d4f6d31141e1353c6873b68b04d
theperacar <petricpetar22@gmail.com>
2020-07-06 14:27:08 +0200

    Added functionality for X and O

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
+
+const board = document.querySelectorAll('.cell');
+let playerOneTurn = true;
+
+const playerRound = () => {
+  playerOneTurn = !playerOneTurn
+}
+
 const gameBoardObj = (() => {
   const div1 = document.getElementById('b1');
   const div2 = document.getElementById('b2');
@@ -8,10 +16,66 @@ const gameBoardObj = (() => {
   const div7 = document.getElementById('b7');
   const div8 = document.getElementById('b8');
   const div9 = document.getElementById('b9');
-
+  
   const gameBoardArr = [div1, div2, div3, div4, div5, div6, div7, div8, div9];
-  console.log(gameBoardArr);
 
+  board.forEach(button => {
+    button.addEventListener('click', (e) => {
+    if(playerOneTurn === true && e.target.innerHTML === ""){
+      e.target.innerHTML = "X"
+      playerRound();
+    } else if(e.target.innerHTML === "") {
+      e.target.innerHTML = "O"
+      playerRound();
+    }            
+    });
+  });
+  return gameBoardArr
 })();
 
-//const playerobj 
+let currBoardArr = gameBoardArr;
+
+console.log(currBoardArr);
+
+const playerFac = (player, score) => {
+  score = 0;
+  return {player, score};
+};
+
+const btnControl = (() => {
+  const startBtn = document.querySelector('.play');
+  const playerField1 = document.getElementById('player1');
+  const playerField2 = document.getElementById('player2');
+  const restartBtn = document.querySelector('.restart');
+  const gameBox = document.getElementById('game-box');
+
+  const sumResults = [];
+
+  startBtn.addEventListener('click', () => {
+    console.log(sumResults);
+      const player1 = playerFac(
+        playerField1.value,
+        
+      );
+      const player2 = playerFac (
+        playerField2.value,
+      );
+      
+      sumResults.push(player1, player2);
+      startBtn.style.display = "none";
+      restartBtn.style.display = "flex";
+      gameBox.style.display = "flex";
+
+  })
+  restartBtn.addEventListener('click', () => {
+    console.log(sumResults);
+    startBtn.style.display = "flex";
+    restartBtn.style.display = "none";
+    sumResults.pop();
+    sumResults.shift();
+    
+  })
+  
+})();
+
+
